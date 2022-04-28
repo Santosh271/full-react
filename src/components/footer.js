5ea8445a2effbbf5d881b0b99c9299c52e8453a8
santosh271 <santoshnagpure70000@gmail.com>
2022-04-28 09:42:44 +0530

    video five

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+   <footer className='bg-dark text-light py-3'>
+    <p className='text-center'>
+
+      Copyright &2022 : Sunny Sarkar:- 
+    </p>
+
+   </footer>
+  )
+}
+
+export default footer
